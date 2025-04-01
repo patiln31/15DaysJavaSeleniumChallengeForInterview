5478dcf3f91205e5bec2feac6a594e569e6f692f
Patil Nilesh <patiln31@gmail.com>
2025-04-01 14:50:22 +0530

    Day 1 final

--- src/main/java/Day1/FirstNonRepeatingCharacterString.java
+++ src/main/java/Day1/FirstNonRepeatingCharacterString.java
@@ -1,7 +1,28 @@
 package Day1;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FirstNonRepeatingCharacterString {
     public static void main(String[] args) {
-        String str = "automation";
+        String input = "automation";
+        System.out.println("First non-repeating character: " + firstNonRepeatingChar(input));
+    }
+
+
+    public static char firstNonRepeatingChar(String str) {
+        Map<Character, Integer> charCount =  new LinkedHashMap<>();
+
+        for (char ch : str.toCharArray()){
+            charCount.put(ch, charCount.getOrDefault(ch,0)+1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()){
+            if (entry.getValue()==1){
+                return entry.getKey();
+            }
+        }
+
+        return '_'; // If all characters repeat
     }
 }
